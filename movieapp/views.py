5e1f278ba217c751cc37a4d14d10b4fbbf8bc9e6
AbdulKareem-M <abdulkareemyousaf1245@gmail.com>
2025-01-27 19:38:37 +0530

    added a view for adding movies to the model through a  template and created a  url for it

--- movieapp/views.py
+++ movieapp/views.py
@@ -2,4 +2,26 @@ from django.shortcuts import render
 
 from django.views.generic import View
 
+from movieapp.forms import MovieForm
+from movieapp.models import MovieModel
+# Create your views here.
+
+
+class AddMovie(View):
+  
+  def get(self, request):
+    
+    form = MovieForm
+    
+    return render(request, 'addmovie.html', {'form':form})
+  
+  def post(self, request):
+    
+    form = MovieForm(request.POST)
+    
+    if form.is_valid():
+      
+      MovieModel.objects.create(**form.cleaned_data)
+      
+      return render(request, 'addmovie.html', {'form':form})
     
--- movies/urls.py
+++ movies/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from movieapp.views import AddMovie
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('addmovie/', AddMovie.as_view()),
 
 ]
